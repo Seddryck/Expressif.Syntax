c9ed3ea48fd5e5352039e623ea99d19ac9e51a85
Cédric L. Charlier <seddryck@gmail.com>
2026-08-26 08:25:48 +0200

    feat(csharp): expose syntax internals to testing (#112)

--- bindings/csharp/Expressif.Syntax.Tests/BuildInfrastructureTests.cs
+++ bindings/csharp/Expressif.Syntax.Tests/BuildInfrastructureTests.cs
@@ -7,4 +7,19 @@ public class BuildInfrastructureTests
     {
         Assert.That(typeof(AssemblyMarker).Assembly.GetName().Name, Is.EqualTo("Expressif.Syntax"));
     }
+
+    [Test]
+    public void ProductionAssemblyExposesInternalsToTestingAssemblies()
+    {
+        var friendAssemblies = typeof(AssemblyMarker).Assembly
+            .GetCustomAttributes(
+                typeof(System.Runtime.CompilerServices.InternalsVisibleToAttribute),
+                inherit: false)
+            .Cast<System.Runtime.CompilerServices.InternalsVisibleToAttribute>()
+            .Select(attribute => attribute.AssemblyName);
+
+        Assert.That(
+            friendAssemblies,
+            Is.EquivalentTo(["Expressif.Syntax.Tests", "Expressif.Testing"]));
+    }
 }
